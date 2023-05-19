215d822d4d1a474c0c9e058eece1b963fdddf3d8
Vinay Kumar <vinaykumar@Vinays-MacBook-Air.local>
2023-05-20 01:32:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:myapp/page/addpage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FreeCode Spot',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
+      ),
+      home: AddPage(),
+    );
+  }
+}
